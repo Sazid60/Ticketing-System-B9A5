4c81ad1d6f1c5f679d55d00bf9df9693265bbe47
Sazid60 <shahnawazsazid60@gmail.com>
2024-02-18 16:14:15 +0600

    Seat Left & Seat Count Section Added

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,7 @@
 // Common Functions
 
 // Get Element value
-function getElementText(elementId) {
+function getElementValue(elementId) {
     const elementField = document.getElementById(elementId);
     const elementInnerText = elementField.innerText;
     const elementInnerValue = parseInt(elementInnerText)
@@ -17,15 +17,36 @@ function setElementText(elementId, value) {
 }
 
 // ______________________________________________Main Functionality_____________________________________
+let count = 0;
 const targetElements = document.getElementsByClassName('seats');
-// console.log(targetElements)
 
 for (const element of targetElements) {
-    // console.log(element.innerText)
-    element.addEventListener('click', function(){
-        element.style.backgroundColor ="green"
-    })
+    element.addEventListener('click', handleSelect);
 }
+
+function handleSelect(event) {
+    const element = event.currentTarget;
+    element.style.backgroundColor = "green";
+    element.classList.add('disable');
+    count++;
+    setElementText('seat-count', count)
+    const seatDeductedValue = getElementValue('seat-deduct') - 1;
+    console.log(seatDeductedValue)
+    setElementText('seat-deduct', seatDeductedValue)
+
+
+    // button disable after 4 click
+    const clickedCount = getElementValue('seat-count')
+
+    if (clickedCount >= 4) {
+        alert('You cannot buy more than 4 tickets');
+        for (const element of targetElements) {
+            element.removeEventListener('click', handleSelect);
+        }
+    }
+}
+
+
 
 
 
